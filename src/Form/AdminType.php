992f21bfab3c80e430169b26bdea3ab13018ef4a
roman <romanbajireanu@gmail.com>
2019-04-29 18:08:09 +0100

    Add Admin and SuperUser

--- src/Form/AdminType.php
+++ src/Form/AdminType.php
@@ -4,31 +4,60 @@ namespace App\Form;
 
 use App\Entity\Admin;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\CallbackTransformer;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\Form\FormEvent;
-use Symfony\Component\Form\FormEvents;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
-
 
 class AdminType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-        ->add('address', TextType::class)
+        ->add('address', TextType::class, array(
+                'label'=>'address',
+                'required' => true,
+                'attr' => ['class' => 'w3-input w3-padding-16','placeholder'=>'address']
+            ))
+            ->add('location', TextType::class, array(
+                'label'=>'location',
+                'required' => true,
+                'attr' => ['class' => 'w3-input w3-padding-16','placeholder'=>'location']
+            ))
+            ->add('postalCode', TextType::class, array(
+                'label'=>'Postal Code',
+                'required' => true,
+                'attr' => ['class' => 'w3-input w3-padding-16','placeholder'=>'Postal Code']
+            ))
+            ->add('nif', TextType::class, array(
+                'label'=>'NIF',
+                'required' => true,
+                'attr' => ['class' => 'w3-input w3-padding-16','placeholder'=>'NIF']
+            ))
+            ->add('email', EmailType::class,
+            array(
+                'label'=>'email',
+                'required' => true,
+                'attr' => ['class' => 'w3-input w3-padding-16','placeholder'=>'email',]
+            ))
+            ->add('username', TextType::class, array(
+                'label'=>'username',
+                'required' => true,
+                'attr' => ['class' => 'w3-input w3-padding-16','placeholder'=>'username','autofocus'=>'autofocus']
+            ))
+            ->add('plainPassword', RepeatedType::class, array(
+                'type' => PasswordType::class,
+                'first_options'  => array('label' => 'Password *', 'attr' => ['class' => 'w3-input w3-padding-16','placeholder'=>'Password *']),
+                'second_options' => array('label' => 'Repetir Password *', 'attr' => ['class' => 'w3-input w3-padding-16','placeholder'=>'Repetir Password *'])
+            ))
         ;
     }
-    
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults(array(
             'data_class' => Admin::class,
-                'user' => null,
         ));
     }
 }
